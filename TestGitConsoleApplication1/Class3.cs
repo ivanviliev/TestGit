dc5135a2b3bff23580f961cc65280d5bffcd9f93
ivan.iliev <ivan.iliev@smartit.bg>
2018-12-07 16:28:25 +0200

    NewClass3

---
+++ TestGitConsoleApplication1/Class3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitConsoleApplication1
+{
+    class Class3
+    {
+        //Class3 in New Branch
+    }
+}
