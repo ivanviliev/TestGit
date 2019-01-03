5439435985a853e225730ff81aeaf10c95775247
ivan.iliev <ivan.iliev@smartit.bg>
2019-01-03 11:57:30 +0200

    branch2 class 1 and class 2

---
+++ TestGitConsoleApplication1/br1Clas2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitConsoleApplication1
+{
+    class br1Clas2
+    {
+        // branch1 class2
+    }
+}
---
+++ TestGitConsoleApplication1/branch1Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitConsoleApplication1
+{
+    class branch1Class1
+    {
+        //new class in new branch Test1
+    }
+}
